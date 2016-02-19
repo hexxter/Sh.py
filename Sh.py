a71b3d775e20b45f8474991a8f292b89ed3b0221
Alexander Kratzer <alexander.kratzer@semsotec.de>
2016-02-19 10:03:39 +0100

    repair

--- Sh.py
+++ Sh.py
@@ -1,61 +1,55 @@
-#!/usr/bin/python3
-
 import sys
 from subprocess import Popen, PIPE
 
-class Shell:
 
-	def __init__( self, command, *args, trusted ):
+class Shell:
+	def __init__(self, command, trusted, *args):
 
 		assert command
 
 		self._command = command
 		self._args = []
-		#print( self._args, args )
-		self._args.extend( args )
-		#print( self._args )
+		# print( self._args, args )
+		self._args.extend(args)
+		# print( self._args )
 		self._trusted = trusted
 		self._encoding = sys.getdefaultencoding()
 		self._bufsize = 4096
 
+	def _the_args_escaped(self):
 
-	def _the_args_escaped( self ):
-
-		if( self._args ):
-			return " ".join( "'" + it + "'" for it in self._args )
+		if (self._args):
+			return " ".join("'" + it + "'" for it in self._args)
 		else:
 			return ""
 
-	def _the_args( self ):
+	def _the_args(self):
 
-		if( self._args ):
-			return " ".join( self._args )
+		if (self._args):
+			return " ".join(self._args)
 		else:
 			return ""
 
-	def _the_command( self ):
+	def _the_command(self):
 
 		if self._args:
 			return self._command + " " + self._the_args()
 		else:
 			return self._command
 
-
-	def run( self ):
+	@property
+	def run(self):
 
 		with self.process() as proc:
-
 			out = proc.stdout.read()
-			#err = proc.stderr.read()
+		# err = proc.stderr.read()
 
-
-		outS = str( out, self._encoding )
-		errS = "" #str( err, self._encoding )
+		outS = str(  out, self._encoding  )
+		errS = ""  # str( err, self._encoding )
 
 		return (outS, errS)
 
-
-	def process( self, preproc=None ):
+	def process(self, preproc=None):
 
 		stdin = None
 		stdout = PIPE
@@ -64,22 +58,21 @@ class Shell:
 		if preproc != None:
 			stdin = preproc.stdout
 
-		if hasattr( self, '_in' ):
+		if hasattr(self, '_in'):
 			stdin = PIPE
 
-		proc = Popen( self._the_command(),
-				stdin=stdin, stdout=stdout, stderr=stderr,
-				bufsize=self._bufsize,
-				shell=True )
+		proc = Popen(self._the_command(),
+					 stdin=stdin, stdout=stdout, stderr=stderr,
+					 bufsize=self._bufsize,
+					 shell=True)
 
-		if hasattr( self, '_in' ):
-			proc.stdin.write( self._in.encode( self._encoding ) )
+		if hasattr(self, '_in'):
+			proc.stdin.write(self._in.encode(self._encoding))
 			proc.stdin.close()
 
-		if hasattr( self, '_out' ):
-			if hasattr( self._out, 'process' ):
-
-				subproc = self._out.process( proc )
+		if hasattr(self, '_out'):
+			if hasattr(self._out, 'process'):
+				subproc = self._out.process(proc)
 
 				proc.stdout.close()
 
@@ -87,96 +80,81 @@ class Shell:
 
 		return proc
 
+	def arg(self, argument):
 
-	def arg( self, argument ):
+		self._args.append(argument)
 
-		self._args.append( argument );
-
-		#print( self._args )
+		# print( self._args )
 
 		return self
 
-
-	def encoding( self, encoding ):
+	def encoding(self, encoding):
 
 		self._encoding = encoding
 
 		return self
 
-
-	def bufsize( self, newsize ):
+	def bufsize(self, newsize):
 
 		self._bufsize = newsize
 
 		return self
 
+	def __str__(self):
 
-	def __str__( self ):
-
-		(out,err) = self.run()
+		(out, err) = self.run
 
 		return out
 
-
-	def __repr__( self ):
+	def __repr__(self):
 
 		return self._the_command()
 
+	def __add__(self, other):
 
-	def __add__( self, other ):
+		return self.arg(other)
 
-		return self.arg( other )
+	def __ror__(self, other):
 
-
-	def __ror__( self, other ):
-
-		#print( "=ROR=", type( other ), "|", self._command  )
+		# print( "=ROR=", type( other ), "|", self._command  )
 
 		self._in = other
 
 		return self
 
+	def __or__(self, other):
 
-	def __or__( self, other ):
+		# print( "=OR=", self._command, type( other ) )
 
-		#print( "=OR=", self._command, type( other ) )
+		if hasattr(other, '__call__'):
 
-		if hasattr( other, '__call__' ):
-
-			#print( "==CALL==" )
-			return other( self.__str__() )
+			# print( "==CALL==" )
+			return other(self.__str__())
 
 		else:
 
-			if not hasattr( self, '_out ' ):
-				#print( "==STORE==" )
+			if not hasattr(self, '_out '):
+				# print( "==STORE==" )
 				self._out = other
 			else:
-				if hasattr( self._out, '__or__' ):
-					#print( "==STORE==" )
-					self._out.__or__( other )
+				if hasattr(self._out, '__or__'):
+					# print( "==STORE==" )
+					self._out.__or__(other)
 				else:
-					raise AttributeError( 'Tried to chain the unchainable' )
+					raise AttributeError('Tried to chain the unchainable')
 
 			return self
 
 
+class Sh(Shell):
+	# "Execute Shell commands in a insane way"
+
+	def __init__(self, command, *args):
+		super(Sh, self).__init__(command, *args, trusted=False)
 
 
-class Sh( Shell ):
+class sh(Shell):
+	# "Execute Shell commands in a sane way"
 
-	#"Execute Shell commands in a insane way"
-
-	def __init__( self, command, *args ):
-
-		super( Sh, self ).__init__( command, *args, trusted=False )
-
-
-class sh( Shell ):
-
-	#"Execute Shell commands in a sane way"
-
-	def __init__(self, command, *args, trusted):
-
-		super( Sh, self ).__init__( command, *args, trusted=True )
-
+	def __init__(self, command, *args):
+		super(Sh, self).__init__(command, *args, trusted=True)
